2855bcb5b20bbcff413a2849783d5ad3d034497d
Deshan Ruhunage <deshan.r@fidenz.com>
2020-05-26 15:35:13 +0530

    Removed app level routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,3 @@
 Rails.application.routes.draw do
   root 'welcome#index'
-  namespace 'v1', defaults: { format: :json } do
-    get '/upload/init' => 'large_file_upload#initialize_upload'
-    post '/upload' => 'large_file_upload#upload'
-    get '/upload/status' => 'large_file_upload#upload_status'
-  end
 end

52e5d7825d684bedd26dc30800c550083cc498f0
Deshan Ruhunage <deshan.r@fidenz.com>
2020-06-22 11:42:51 +0530

    FZLFU-17 Restrict access with cookie.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_filter :maintainance_message_redirection
+
+  def maintainance_message_redirection
+  # unless allowed_ips.any? { |block| block.include?(request.remote_ip) }
+  redirect_to '/404.html' unless cookies[:lfu_845TY8].present?
+  # end
+end
 end
